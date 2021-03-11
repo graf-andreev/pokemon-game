2a7890d9637ede53f382154941e4bdd96b397a65
Maxim Andreev <maxim.andreev@legionfarm.com>
2021-03-11 23:58:19 +0700

    first lesson homework

---
+++ pokemon/src/components/Layout/Layout.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from './Layout.module.css'
+
+const Layout = ({title, desc, urlBg, colorBg}) => {
+    return(
+        <section style={urlBg? {background: `url(${urlBg})`} : {background: colorBg}} className={styles.root}>
+            <div className={styles.wrapper}>
+                <article>
+                    <div className={styles.title}>
+                        <h3>{title}</h3>
+                        <span className={styles.separator}></span>
+                    </div>
+                    <div className={`${styles.desc} ${styles.full}`}>
+                        <p>{desc}</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    )
+}
+
+export default Layout
